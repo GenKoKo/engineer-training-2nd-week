5ec9fb9a491c32a39df2dd0613a0e40c6748d038
Ko <orangeko@gmail.com>
2021-03-05 10:22:13 +0900

    20210305 10 AM python Q1,2 done

--- python 2nd week.py
+++ python 2nd week.py
@@ -1,1 +1,42 @@
-print("!!!")
+# Q1
+print("---Q1---")
+
+def calculate(min, max):
+    total = 0
+    while min <= max:
+        total += min
+        min += 1
+    
+    print (total)
+
+
+calculate(1,3)
+calculate(4,8)
+
+
+# Q2
+print("---Q2---")
+
+def avg(data):
+
+    total = 0
+    employees_no = 0
+    while employees_no < len(data["employees"]):
+
+        total += data["employees"][employees_no]["salary"]
+        employees_no += 1
+    
+    average = total / len(data["employees"])
+
+    print(average)
+    
+    
+avg({"count":3,
+    "employees":[{"name":"John","salary":30000},
+                {"name":"Bob","salary":60000},
+                {"name":"Jenny","salary":50000}]
+    })
+
+
+# Q3
+print("---Q3---")
